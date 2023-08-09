5445faf1ada3f8697ceb2e4661a721ca20c41747
Kowther Kahin <kowtherkahin@Kowthers-MacBook-Pro.local>
2023-08-09 07:25:59 -0700

    set up rendering to DOM of current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 var cityInfo = document.querySelector(".cityInfo")
+var icon = document.querySelector(".icon")
 var temp = document.querySelector(".temp")
 var wind = document.querySelector(".wind")
 var hum = document.querySelector(".hum")
 var searchBtn = document.querySelector(".submitBtn")
 var searchBar = document.querySelector(".searchBar")
 var apiKey = "6b5170895ce23f4cc7318517a14c4f41"
+//var history = JSON.parse(localStorage.getItem("cities")) || []
 
 searchBtn.addEventListener("click", function() {
     var cityName = searchBar.value
+    //history.push(cityName)
+    //localStorage.setItem("cities", cityName)
     getCurrentData(cityName)
 })
 
@@ -18,5 +22,15 @@ function getCurrentData(cityName) {
     })
     .then(function(data) {
         console.log(data)
+        var date = new Date()
+        var month = date.getMonth() + 1
+        var day = date.getDate()
+        var year = date.getFullYear()
+        cityInfo.innerHTML = data.name + " - " + month + "/" + day + "/" + year
+        icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
+        icon.setAttribute("alt", data.weather[0].description)
+        temp.innerHTML = data.main.temp
+        wind.innerHTML = data.wind.speed
+        hum.innerHTML = data.main.humidity
     })
 }

bb4b2dc9d66e5eae0d4a5adc2c5aa06e12fa6c42
Kowther Kahin <kowtherkahin@Kowthers-MacBook-Pro.local>
2023-08-09 07:13:07 -0700

    setup first function to fetch current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,21 @@ var cityInfo = document.querySelector(".cityInfo")
 var temp = document.querySelector(".temp")
 var wind = document.querySelector(".wind")
 var hum = document.querySelector(".hum")
-var searchForm = document.querySelector(".searchForm")
+var searchBtn = document.querySelector(".submitBtn")
 var searchBar = document.querySelector(".searchBar")
+var apiKey = "6b5170895ce23f4cc7318517a14c4f41"
+
+searchBtn.addEventListener("click", function() {
+    var cityName = searchBar.value
+    getCurrentData(cityName)
+})
+
+function getCurrentData(cityName) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial")
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+}
